7eb50aa5d603b84c98ff3e1c1b94bd3fd9bb9d40
Mariano Guerra <luismarianoguerra@gmail.com>
2012-06-26 16:41:55 +0200

    generate atom and rss feeds of the changes

--- releases.py
+++ releases.py
@@ -3,9 +3,70 @@
 import os
 import csv
 import json
+import collections
+
 import requests
 
 import feedformatter
+
+FEED_SIZE = 100
+RELEASES_LIMIT = 100
+RELEASES_PATH = "cache/releases"
+
+
+def package_to_feed_entry(package_path):
+    """read a package and return the corresponding feed entry"""
+
+    with open(package_path) as handle:
+        pkg = json.load(handle)
+
+        item = {}
+        item["title"] = "%s %s" % (pkg.get("name", ""), pkg.get("version", ""))
+        item["link"] = pkg.get("homepage", pkg.get("url", ""))
+        item["description"] = pkg.get("description", "")
+        item["guid"] = "/".join(package_path.split("/")[2:])
+
+        return item
+
+def truncate_release_file(path=RELEASES_PATH, count=RELEASES_LIMIT):
+    """leave only the last *count* lines in release file, return the lines
+    in a list"""
+    deque = collections.deque(maxlen=count)
+    line_count = 0
+
+    with open(path) as handle:
+        for line in handle:
+            print(line)
+            deque.append(line.strip())
+            line_count += 1
+
+    print("release lines", line_count)
+
+    lines = []
+    with open(path, "w") as handle:
+        for package_path in deque:
+            handle.write(package_path + "\n")
+
+            lines.append(package_path)
+
+    return lines
+
+def generate_feeds(count=FEED_SIZE):
+    """generate the feed for the last *count* updates"""
+    entries = [package_to_feed_entry(pkg_path) for pkg_path in
+            truncate_release_file(count=count)]
+
+    entries.reverse()
+
+    feed = feedformatter.Feed(items=entries)
+
+    feed.feed["title"] = "releases!"
+    feed.feed["link"] = "http://marianoguerra.github.com/releases/"
+    feed.feed["author"] = "Mariano Guerra"
+    feed.feed["description"] = "library release updates"
+
+    feed.format_rss2_file("rss.xml", True, True)
+    feed.format_atom_file("atom.xml", True, True)
 
 def make_url(repo, org, name, branch):
     '''return the url to locate package.json for the given repo'''
@@ -99,6 +160,8 @@ def check_updates_from_file(path):
                     no_package_json.append(name)
             else:
                 print("invalid project", project)
+
+    generate_feeds()
 
     if no_package_json:
         print("there are projects without a package.json")
